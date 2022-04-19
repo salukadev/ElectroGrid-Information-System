3eb0bb4aca32923392c08c06b3c4cff80cfb6203
Esala Senarathna <esalakalshan@gmail.com>
2022-04-20 00:53:34 +0530

    SupplyConnection : CRUD opereations

--- SupplyConnection/src/com/electrogrid/supplyconnection/model/SupplyConnection.java
+++ SupplyConnection/src/com/electrogrid/supplyconnection/model/SupplyConnection.java
@@ -2,9 +2,13 @@ package com.electrogrid.supplyconnection.model;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.sql.Timestamp;
+import java.util.ArrayList;
 
 import com.electrogrid.supplyconnection.utils.DBConnectionSingleton;
+
 
 public class SupplyConnection {
 	
@@ -136,13 +140,71 @@ public class SupplyConnection {
 		
 
 	//update supply connection
-	public String updateConnection() {
-		return "";
+	public String updateConnection(int accNo, String name, String address, 
+			String area, String type, String connection_status) {
+		String output = ""; 
+		try
+		{ 
+			if (con == null) 
+			{
+				return "Error while connecting to the database for updating."; 
+			} 
+			// create a prepared statement
+			String query = "UPDATE supply_connection SET name=?, address=?, area=?, type=?,connection_status=?, timestamp=? WHERE accNo=?"; 
+			PreparedStatement preparedStmt = con.prepareStatement(query); 
+			// binding values
+			
+			//Prepare sql timestamp
+			final java.util.Date today = new java.util.Date();
+		   	final java.sql.Timestamp todaySQL = new java.sql.Timestamp(today.getTime());
+			
+			
+			preparedStmt.setString(1, name); 
+			preparedStmt.setString(2, address); 
+			preparedStmt.setString(3, area);
+			preparedStmt.setString(4, type);
+			preparedStmt.setString(5, connection_status);
+			preparedStmt.setTimestamp(6, todaySQL);
+			preparedStmt.setInt(7, accNo);
+			// execute the statement
+			preparedStmt.execute(); 
+			output = "Connection Updated successfully"; 
+		} 
+		catch (Exception e) 
+		{ 
+			output = "Error while updating the connection."; 
+			System.err.println(e.getMessage()); 
+		} 
+		return output;
 	}
 
 	//delete supply connection
-	public String deleteConnection() {
-		return "";
+	public String deleteConnection(Integer accNo) {
+		String output = "";
+		try{
+			 if (con == null)
+			 {return "Processing Error!"; }
+		 
+			 // create a statement and execute query
+			 String query = "delete from supply_connection where accNo=?";
+			 PreparedStatement preparedStatement = con.prepareStatement(query);
+			 preparedStatement.setInt(1,accNo);
+			 int state = preparedStatement.executeUpdate();
+			 //System.out.println(state);
+			 if(state==0) {
+				 output = "Connection with Account No :"+ accNo +" does not exist!";
+			 }else {
+				 output = "Connection with the Account No :"+ accNo +" is Deleted successfully !";
+			 }
+			 
+			
+		}
+		catch (Exception e)
+		{
+			System.err.println(e.getMessage());
+			output = "Processing Error!";
+		} 	
+		return output;
 	}
 		
 		
@@ -153,8 +215,36 @@ public class SupplyConnection {
 		
 		
 	//view all the connections
-	public String viewAllConnections() {
-		return "";
+	public ArrayList<SupplyConnection> viewAllConnections() {
+		ArrayList<SupplyConnection> SupplyConnectionList = new ArrayList<SupplyConnection>();
+		try{
+			if (con == null){
+				 return SupplyConnectionList; 
+			}
+		 
+			 // create a statement and execute query
+			String query = "SELECT * FROM supply_connection";
+			Statement statement = con.createStatement(); 
+			ResultSet rs = statement.executeQuery(query);
+			 
+			while (rs.next()){
+		        SupplyConnectionList.add(new SupplyConnection(
+		        		rs.getInt("accNo"),
+				        rs.getString("name"),
+				        rs.getString("address"),
+				        rs.getString("area"),
+				        rs.getString("type"),
+				        rs.getString("connection_status"),
+				        rs.getTimestamp("timestamp") 
+		        ));
+		    }
+		 }
+		 catch (Exception e)
+		 {
+			 System.err.println(e.getMessage());
+			 
+		 } 	
+		return SupplyConnectionList;
 	}
 		
 		
