e0ef06be2e25f1492a1719db34835ac0aedad864
parami lelkada <lelkadaparami@gmail.com>
2022-04-19 16:40:33 +0530

    consumption: method to geh consumption history

--- ConsumptionService/src/main/java/com/electrogrid/consumption/model/Consumption.java
+++ ConsumptionService/src/main/java/com/electrogrid/consumption/model/Consumption.java
@@ -2,11 +2,14 @@ package com.electrogrid.consumption.model;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import com.electrogrid.consumption.utils.DBConnectionSingleton;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import com.electrogrid.consumption.model.Consumption;
 
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.electrogrid.consumption.utils.DBConnectionSingleton;
 
 public class Consumption {
 	private int consumptionId;
@@ -14,10 +17,23 @@ public class Consumption {
 	private int year;
 	private int month;
 	private int units;
+	private float calculatedBal;
 	
 	
 	static Connection con = DBConnectionSingleton.getConnection();
 	
+	public Consumption(int consumptionId, int accNo, int year, int month, int units, float calculatedBal) {
+		super();
+		this.consumptionId = consumptionId;
+		this.accNo = accNo;
+		this.year = year;
+		this.month = month;
+		this.units = units;
+		this.calculatedBal = calculatedBal;
+	}
+
+	public Consumption() {}
+
 	public int getConsumptionId() {
 		return consumptionId;
 	}
@@ -58,36 +74,48 @@ public class Consumption {
 		this.units = units;
 	}
 	
+	public float getCalculatedBal() {
+		return calculatedBal;
+	}
 	
-	public String consumptionHistoryAll() {
-		if (con == null) 
-		 {return "Error while connecting to the database for updating."; }
+	public void setCalculatedBal(float calculatedBal) {
+		this.calculatedBal = calculatedBal;
+	}
+	
+	
+	
+	public ArrayList<Consumption>  consumptionHistoryAll() {
+		ArrayList<Consumption> consumptionHistory = new ArrayList<Consumption>();
+		try
+		 {
+			 if (con == null)
+			 {return consumptionHistory; }
 		 
-		String output = "<table border='1'><tr><th>ConsumptionID</th><th>AccountNo</th>";
-		try {
-			//output += "inside try";
-			String query = "select * from consumption"; 
-			Statement stmt = con.createStatement();
-			
-			ResultSet res = stmt.executeQuery(query); 
-			
-			while (res.next()) {
-				
-				String consumptionID = Integer.toString(res.getInt("consumptionId")); 
-				String accNO = Integer.toString(res.getInt("accNo"));
-				
-				output += "<tr><td>" + consumptionID + "</td>";
-				output += "<td>" + accNO + "</td></tr>";
-				
-			}
-			output += "</table>";
-		}
-		catch (SQLException e) {
-			// TODO Auto-generated catch block
-			System.out.print(e);
-			e.printStackTrace();
-			
-		} 
-		return output;
+			 // create a statement and execute query
+			 String query = " SELECT * FROM consumption;";
+			 Statement st = con.createStatement(); 
+			 ResultSet rs = st.executeQuery(query);
+			 
+			 while (rs.next())
+		      {
+		        this.consumptionId = rs.getInt("consumptionId");
+		        this.accNo  = rs.getInt("accNo");
+		        this.year = rs.getInt("year");
+		        this.month = rs.getInt("month");
+		        this.units = rs.getInt("units");
+		        this.calculatedBal = rs.getFloat("calculatedBal");
+		       
+		        
+		        //create Consumption object from db entries
+		        Consumption trans = new Consumption(consumptionId, accNo, year, month, units, calculatedBal);
+		        consumptionHistory.add(trans);
+		        
+		      }
+		 }
+		 catch (Exception e)
+		 {
+			 System.err.println(e.getMessage());
+		 }
+		return consumptionHistory; 
 	}
 }
