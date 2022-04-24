b709aec00ca34f9f89c38e049213325317b945ae
salukadev <salukadev4@gmail.com>
2022-04-24 02:01:00 -0700

    Billing: Bill creation done with bill by id method

--- BillingService/src/main/java/com/electrogrid/billing/service/BillingService.java
+++ BillingService/src/main/java/com/electrogrid/billing/service/BillingService.java
@@ -5,6 +5,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.electrogrid.billing.model.Bill;
+
 @Path("/billing")
 public class BillingService {
 	@GET
@@ -13,5 +15,16 @@ public class BillingService {
 	public String hello()
 	 {
 		return "Service is active!";
+	 }
+	
+	@GET
+	@Path("/new")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String newBill()
+	 {
+		Bill bill = new Bill();
+		String output = bill.insertRecord(199, 320, 4000.35f);
+		return output;
+		
 	 }
 }
