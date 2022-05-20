646c30b8632a18680cc1a9e463a672834b0078e7
salukadev <salukadev4@gmail.com>
2022-05-20 21:06:25 +0530

    Billing: Bill creation done with bill by id method

--- BillingService/src/main/java/com/electrogrid/billing/model/Bill.java
+++ BillingService/src/main/java/com/electrogrid/billing/model/Bill.java
@@ -1,16 +1,24 @@
 package com.electrogrid.billing.model;
 
+import java.sql.Connection;
 import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import com.electrogrid.billing.utils.DBConnectionSingleton;
 
 public class Bill {
 	private Integer billId;
 	private Integer accNo;
-	private Integer consumptionId;
+	private Integer totUnits;
 	private java.sql.Date period;
 	private Float previousBal;
 	private Float calculatedBal;
 	private Float totPay;
 	private Float totalDue;
+	
+	static Connection con = DBConnectionSingleton.getConnection();
 	
 	public Bill() {}
 	
@@ -18,7 +26,7 @@ public class Bill {
 			Float totalDue) {
 		super();
 		this.accNo = accNo;
-		this.consumptionId = consumptionId;
+		this.totUnits = consumptionId;
 		this.period = period;
 		this.previousBal = previousBal;
 		this.calculatedBal = calculatedBal;
@@ -27,7 +35,132 @@ public class Bill {
 	}
 	
 	//method to create records
+	public String insertRecord(int accNo, int units, float amount)
+	{
+		String output = "";
+		try
+		 {
+			 if (con == null)
+			 {return "DB Error!"; }
+		 
+			 // Check for the previous bills of the account
+			 String query = "SELECT * FROM bills WHERE accNo=? ORDER BY billId DESC LIMIT 1";
+			 PreparedStatement pSt = con.prepareStatement(query);
+			 pSt.setInt(1,accNo);
+			 ResultSet rs = pSt.executeQuery();
+			 
+			 boolean read = false;
+			 
+			 while (rs.next())
+		      {
+				read=true;
+				this.billId = rs.getInt("billId") ;
+		        this.accNo = rs.getInt("accNo") ;
+		        this.totUnits = rs.getInt("totUnits");
+		        this.period = rs.getDate("period");
+		        this.previousBal = rs.getFloat("previousBal");
+		        this.calculatedBal = rs.getFloat("calculatedBal");
+		        this.totPay = rs.getFloat("totPay");
+		        this.totalDue = rs.getFloat("totalDue");
+		      }
+			 
+			//prepare today's date in sql date format
+			 final java.util.Date today = new java.util.Date();
+			 java.sql.Date sqlToday = new java.sql.Date(today.getTime());
+			 
+		      //If no prev bills found for the accNo
+			 if(!read) {
+				 System.out.println("No previous bills for this connection ! Creating the first bill");
+				 
+				 String queryIn = " insert into bills values (0,?, ?, ?, ?, ?, ?, ?)"; 
+				 PreparedStatement preparedSt = con.prepareStatement(queryIn);
+				 
+				 
+				// binding values
+				 preparedSt.setInt(1, accNo); //accNo
+				 preparedSt.setInt(2, units);  //totUnits
+				 preparedSt.setDate(3, sqlToday);  //period
+				 preparedSt.setFloat(4, 0f); //previousBal
+				 preparedSt.setFloat(5, amount); //calculatedBal
+				 preparedSt.setFloat(6, 0f); //totPay
+				 preparedSt.setFloat(7, amount); //totalDue
+				 
+				// execute the statement
+				 preparedSt.executeUpdate();
 	
+			 }else {
+				 //when previous month's data is available
+				 String queryIn = " insert into bills values (0,?, ?, ?, ?, ?, ?, ?)"; 
+				 PreparedStatement preparedSt = con.prepareStatement(queryIn);
+				 
+				// binding values
+				 preparedSt.setInt(1, accNo); //accNo
+				 preparedSt.setInt(2, units);  //totUnits
+				 preparedSt.setDate(3, sqlToday);  //period
+				 preparedSt.setFloat(4, this.totalDue); //set prev month's DUes to previousBal
+				 preparedSt.setFloat(5, amount); //calculatedBal for this month
+				 preparedSt.setFloat(6, 0f); //totPay
+				 preparedSt.setFloat(7, this.totalDue+amount); //totalDue {prevBal + this month bill}
+				 
+				// execute the statement
+				 preparedSt.executeUpdate();
+				 System.out.println("Created the bill for this month");
+				 
+				 
+			 }
+			 output = "Bill created";
+		 }
+		 catch (Exception e)
+		 {
+			 System.err.println(e.getMessage());
+			 output = "Processing Error!";
+		 } 	
+		return output;
+	}
+	
+	//get bill by id
+	public Bill getBillById(int bid)
+	{
+		try
+		 {
+			 if (con == null)
+			 {
+				 System.out.println("DB Error!");
+				 return null; 
+			 }
+		 
+			 // Check for the previous bills of the account
+			 String query = "SELECT * FROM bills WHERE billId=?";
+			 PreparedStatement pSt = con.prepareStatement(query);
+			 pSt.setInt(1,bid);
+			 ResultSet rs = pSt.executeQuery();
+			 
+			 boolean read = false;
+			 
+			 while (rs.next())
+		      {
+				read=true;
+				this.billId = rs.getInt("billId") ;
+		        this.accNo = rs.getInt("accNo") ;
+		        this.totUnits = rs.getInt("totUnits");
+		        this.period = rs.getDate("period");
+		        this.previousBal = rs.getFloat("previousBal");
+		        this.calculatedBal = rs.getFloat("calculatedBal");
+		        this.totPay = rs.getFloat("totPay");
+		        this.totalDue = rs.getFloat("totalDue");
+		      }
+			 
+			 if(read) {
+				 System.out.println("Reading bill id: "+bid);
+				 return this;
+			 }
+			 
+		 }catch (Exception e)
+		 {
+			 System.err.println(e.getMessage());
+		 }
+		return null; 	
+	}
 	
 	//getters and setters
 	public Integer getBillId() {
@@ -42,11 +175,11 @@ public class Bill {
 	public void setAccNo(Integer accNo) {
 		this.accNo = accNo;
 	}
-	public Integer getConsumptionId() {
-		return consumptionId;
+	public Integer getTotUnits() {
+		return totUnits;
 	}
-	public void setConsumptionId(Integer consumptionId) {
-		this.consumptionId = consumptionId;
+	public void setTotUnits(Integer totUnits) {
+		this.totUnits = totUnits;
 	}
 	public java.sql.Date getPeriod() {
 		return period;
